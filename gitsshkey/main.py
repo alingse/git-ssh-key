f2299d574e657e1237fb22292a58e4ada47b21a0
alingse <alingse@foxmail.com>
2022-01-30 23:55:04 +0800

    add gen

--- gitsshkey/main.py
+++ gitsshkey/main.py
@@ -1,5 +1,44 @@
+import pathlib
+
 import click
+from giturlparse import parse
 
 
-def main():
-    click.secho('TODO', fg='green')
+class GitRepoType(click.ParamType):
+    name = "repo"
+
+    def convert(self, value, param, ctx):
+        repo = parse(value)
+        if not repo.valid:
+            self.fail(f"{value!r} is not a valid git repo", param, ctx)
+        return repo
+
+
+GIT_REPO = GitRepoType()
+
+
+DEFAULT_SSH_CONFIG = '.ssh/config'
+DEFAULT_SSH_KEYS = '.ssh/keys/'
+
+
+_add_home = lambda p : pathlib.Path().home().joinpath(p).absolute().as_posix()
+
+
+@click.command()
+@click.option('-c', '--config', 'ssh_config',
+    type=click.Path(exists=True, dir_okay=False, file_okay=True, writable=True),
+    help=f'ssh config file path, default is $HOME/{DEFAULT_SSH_CONFIG}',
+    default=_add_home(DEFAULT_SSH_CONFIG))
+@click.option('--keys', 'keys',
+    type=click.Path(exists=True, dir_okay=True, file_okay=False, writable=True),
+    help=f'ssh keys file path, default is $HOME/{DEFAULT_SSH_KEYS}',
+    default=_add_home(DEFAULT_SSH_KEYS))
+@click.argument('repo', type=GIT_REPO)
+def main(repo, keys, ssh_config):
+    '''
+    REPO    git repo https/ssh/git
+    '''
+    click.echo(repo)
+    click.echo(keys)
+    click.echo(ssh_config)
+    click.secho(repo.url2ssh, fg='green')
